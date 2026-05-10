9809b2728246873d2c9409241ae78a0494c713e6
RednekImhotep <michaelmalick2022@gmail.com>
2026-05-10 08:28:04 -0400

    Create config.py

---
+++ config.py
@@ -0,0 +1,9 @@
+CONFIG = {
+    "industry": "HVAC",
+    "intents": ["EMERGENCY", "QUOTE", "GENERAL"],
+    "messages": {
+        "EMERGENCY": "We saw you called and it sounds urgent. A tech is being notified now.",
+        "QUOTE": "We can get you a fast estimate. Reply back and we will set it up.",
+        "GENERAL": "Thanks for calling. We will follow up with you shortly."
+    }
+}
